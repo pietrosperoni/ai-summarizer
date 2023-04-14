c17a86d2d88782fdcd18e8e60a6ab3d28fa83cab
Pietro Speroni di Fenizio <git@piespe.net>
2023-04-14 18:54:06 +0200

    added the long description

--- setup.py
+++ setup.py
@@ -1,4 +1,14 @@
 from setuptools import setup
+from pathlib import Path
+
+# The directory containing this file
+HERE = Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+# This call to setup() does all the work
+
 
 setup(
     name="AI-Summarizer",
@@ -6,6 +16,8 @@ setup(
     description="""AI Summarizer is a Python package that uses OpenAI's GPT-3.5 to summarize any given text.
     You can input some text describing what kind of information you are interested in. 
     When the text is too long it splits it logically into different sections, to make it simpler to summarise""",
+    long_description=README,
+    long_description_content_type="text/markdown",
     url="https://github.com/pietrosperoni/ai-summarizer",
     author="Pietro Speroni di Fenizio",
     author_email="aisummarizer@piespe.net",
