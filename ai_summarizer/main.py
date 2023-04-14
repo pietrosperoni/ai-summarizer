c7b281a9ad7625cc76bb57213311e3061df7d6d3
Pietro Speroni di Fenizio <git@piespe.net>
2023-04-14 18:29:28 +0200

    Add __init__.py file to ai_summarizer package

--- ai_summarizer/main.py
+++ ai_summarizer/main.py
@@ -15,7 +15,7 @@ def get_number_of_tokens(text,model="gpt-3.5-turbo"):
     encoding = tiktoken.encoding_for_model(model)
     return len(encoding.encode(text))
 
-def split_text_around_halfway(A):
+def split_text_logically(A: str):
     """Split the text logically by sections, not by characters. If there is only one section uses subsections and so on.
     It works on markdown text. and it can be used progressively to split the text in smaller and smaller pieces as needed."""
     half_len = len(A) // 2
@@ -45,11 +45,11 @@ def split_text_around_halfway(A):
     part_b=A[split_indices:]
     return part_a, part_b
 
-def recursive_merge_summarises_new(summary_1: str, summary_2: str):
+def _recursive_merge_summarises_new(summary_1: str, summary_2: str):
     result="\n# \n"+summary_1+"\n# \n"+summary_2
     return result
 
-def recursive_summarize_text(text: str, filter: str = None, summary_length: int = 500,model="gpt-3.5-turbo"):
+def _recursive_summarize_text(text: str, filter: str = None, summary_length: int = 500,model="gpt-3.5-turbo"):
     if filter:
         prompt_content = f"""I need you to summarise some text which is part of a bigger document. 
             You should summarise the text by filtering out what does not follows this guidelines: 
@@ -75,10 +75,10 @@ def recursive_summarize_text(text: str, filter: str = None, summary_length: int 
         
     n_tokens=get_number_of_tokens(prompt_content,model=model)
     if n_tokens>4096:
-        text_1,text_2=split_text_around_halfway(text)
-        summary_1=recursive_summarize_text(text_1, filter, summary_length=summary_length,model=model)
-        summary_2=recursive_summarize_text(text_2, filter, summary_length=summary_length,model=model)
-        summary=recursive_merge_summarises_new(summary_1, summary_2)
+        text_1,text_2=split_text_logically(text)
+        summary_1=_recursive_summarize_text(text_1, filter, summary_length=summary_length,model=model)
+        summary_2=_recursive_summarize_text(text_2, filter, summary_length=summary_length,model=model)
+        summary=_recursive_merge_summarises_new(summary_1, summary_2)
         return summary
     
     try:
@@ -89,14 +89,14 @@ def recursive_summarize_text(text: str, filter: str = None, summary_length: int 
     except Exception as e:
         print (e)
         input("Press any key to continue")
-        return recursive_summarize_text(text, filter, summary_length,model=model)
+        return _recursive_summarize_text(text, filter, summary_length,model=model)
     
     finish_reason=response.choices[0].finish_reason
     if finish_reason == "length":
-        text_1,text_2=split_text_around_halfway(text)
-        summary_1=recursive_summarize_text(text_1, filter, summary_length=summary_length,model=model)
-        summary_2=recursive_summarize_text(text_2, filter, summary_length=summary_length,model=model)
-        summary=recursive_merge_summarises_new(summary_1, summary_2)
+        text_1,text_2=split_text_logically(text)
+        summary_1=_recursive_summarize_text(text_1, filter, summary_length=summary_length,model=model)
+        summary_2=_recursive_summarize_text(text_2, filter, summary_length=summary_length,model=model)
+        summary=_recursive_merge_summarises_new(summary_1, summary_2)
         return summary
     summary = response.choices[0].message.content
     return summary
@@ -110,9 +110,9 @@ def html_to_markdown(html_content,ignore_links=True):
 
 def summarize_text(text:str, filter:str=None, summary_length:int=500,model="gpt-3.5-turbo"):
     markdown_content = html_to_markdown(text)
-    summary=recursive_summarize_text(markdown_content, filter, summary_length,model=model)
+    summary=_recursive_summarize_text(markdown_content, filter, summary_length,model=model)
     while (len(summary.split())>summary_length*1.5):
-        summary=recursive_summarize_text(summary, filter,summary_length,model=model)
+        summary=_recursive_summarize_text(summary, filter,summary_length,model=model)
     return summary
 
 
